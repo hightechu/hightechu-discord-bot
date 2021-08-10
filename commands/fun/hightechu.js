4dbe7b77f507e957d27623b95096fdcdd24b0b5f
Dominique Charlebois <domo.charlebois@gmail.com>
2021-08-10 16:36:16 -0700

    Remove Academy from Discord Bot Command.

--- commands/fun/hightechu.js
+++ commands/fun/hightechu.js
@@ -20,10 +20,11 @@ module.exports = {
             .setDescription('Want to learn more about HighTechU?')
             .setThumbnail('https://pbs.twimg.com/profile_images/1349456985557757967/cUPt5g5F_400x400.jpg')
             .addFields(
-                { name: 'HighTechU Skills Academy', value: 'Join us this summer for our 5th Cohort! Visit https://www.hightechu.ca for more information!' },
+                { name: 'HighTechU', value: 'Join us this summer for our 5th, 6th, or 7th Cohort! Visit https://www.hightechu.ca for more information!' },
                 { name: '\u200B', value: '\u200B' },
-                { name: 'August Cohort', value: 'August 20th-21st', inline: true },
-                { name: 'August Cohort', value: 'August 9th-10th', inline: true },
+                { name: 'August Cohort', value: 'August 9th - 13th', inline: true },
+                { name: 'August Cohort', value: 'August 16th - 20th', inline: true },
+                { name: 'August Cohort', value: 'August 23rd - 27th', inline: true },
             )
             .setTimestamp()
             .setFooter('Powered by UVic Engineering', 'https://hightechu.ca/wp-content/uploads/2019/09/logo-2-150x150.png');
